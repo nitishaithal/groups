6f0de60a0a842af0bc7d67345f6a68260d2d9bee
nitishaithal <nitish.k008@gmail.com>
2015-02-24 16:44:15 +0530

    sending mail notification

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,13 @@ Rails.application.configure do
   # Using devise Gem
     config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: "rvce.web@gmail.com",
+      password: "Precious78721"
+    }
 end
